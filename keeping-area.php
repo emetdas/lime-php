50b2c473f59465c7dcf06048b423467cf2fd2fe9
emetdas <emet@b2bleadstaff.com>
2021-05-03 23:07:22 +0600

    all update

---
+++ keeping-area.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$title = "Keeping area";
+    include 'header.php';
+    include 'navbar.php';
+?>
+ <div class="container-fluid m-0">
+      <div class="row">
+        <div class="col-md-3 p-0">
+        <?php include "sidebar.php";?>
+        </div>
+        <div class="col-md-9">
+          <div id="main">
+            <h2 class="text-center pt-3">Keeping area</h2>
+          </div>
+        </div>
+      </div>
+ </div>
+
+<?php
+include 'footer.php'
+?>
