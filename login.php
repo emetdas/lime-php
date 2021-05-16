1931979870b1c37eb4878c1c611fa1a0d2b8b651
emetdas <emet@b2bleadstaff.com>
2021-05-17 00:19:09 +0600

    update

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="height-100 form m-auto w-50 d-flex justify-content-center align-items-center flex-column">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="height-100 form m-auto w-50 d-flex justify-content-center align-items-center flex-column requires-validation">
   <div class="mb-3">
     <label for="email" class="form-label">Email address</label>
     <input type="email" name="email" class="form-control" id="email">
@@ -24,5 +24,24 @@ if (isset($_POST['submit'])) {
     <label for="password" class="form-label">Password</label>
     <input type="password" name="password" class="form-control" id="password">
   </div>
-  <input type="submit" class="btn btn-primary" value="Submit" name="submit">
+  <input type="submit" class="btn bg-primary text-white" value="Submit" name="submit">
 </form>
+<script>
+var forms = document.querySelectorAll('.requires-validation');
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+  .forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  });
+</script>
+<?php
+include 'footer.php';
+?>
