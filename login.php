b8e8a3bac7b3d39a312dab864c41b27a55b64e46
emetdas <emet@b2bleadstaff.com>
2021-05-17 19:21:49 +0600

    update

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include 'header.php';
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    if($email === "emet@limedevs.com" || $password === "limeAdmin"){
+    if($email === "admin@limedevs.com" || $password === "limeAdmin"){
          $_SESSION['login'] = "true";
          $_SESSION['name'] = "Admin";
        header('Location:index.php');
@@ -20,24 +20,16 @@ if (isset($_POST['submit'])) {
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="height-100 form-login m-auto d-flex justify-content-center align-items-center flex-column">
     <div class="mb-3">
       <label for="email" class="form-label">Email address</label>
-      <input type="email" name="email" class="form-control" id="email">
+      <input type="email" name="email" class="form-control" id="email" placeholder=".....@email.com">
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Password</label>
-      <input type="password" name="password" class="form-control" id="password">
+      <input type="password" name="password" class="form-control" id="password" placeholder="......">
     </div>
     <input type="submit" class="btn bg-primary text-white" value="Submit" name="submit">
   </form>
   </div>
 </div>
-<script>
-    var Form = document.querySelector('.form-login');
-    var email = document.querySelector('#email');
-    var password = document.querySelector('#password');
-    Form.addEventListener('submit',(e)=>{
-      e.preventDefault();
-    });
-</script>
 <?php
 include 'footer.php';
 ?>
