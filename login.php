f643df0967c7546a6f273cc200c0f8dea93ee30a
emetdas <emet@b2bleadstaff.com>
2021-05-17 17:16:47 +0600

    update

--- login.php
+++ login.php
@@ -15,32 +15,23 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="height-100 form m-auto w-50 d-flex justify-content-center align-items-center flex-column requires-validation">
-  <div class="mb-3">
-    <label for="email" class="form-label">Email address</label>
-    <input type="email" name="email" class="form-control" id="email">
+<div class="container">
+  <div class="row">
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="height-100 form m-auto d-flex justify-content-center align-items-center flex-column">
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" name="email" class="form-control" id="email">
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" name="password" class="form-control" id="password">
+    </div>
+    <input type="submit" class="btn bg-primary text-white" value="Submit" name="submit">
+  </form>
   </div>
-  <div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" id="password">
-  </div>
-  <input type="submit" class="btn bg-primary text-white" value="Submit" name="submit">
-</form>
+</div>
 <script>
-var forms = document.querySelector('.requires-validation');
-
-// Loop over them and prevent submission
-Array.prototype.slice.call(forms)
-  .forEach(function (form) {
-    form.addEventListener('submit', function (event) {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  });
+  
 </script>
 <?php
 include 'footer.php';
